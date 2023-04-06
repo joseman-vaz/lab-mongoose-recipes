960686b8aa91d855b8ea2870eeb63bfd92f828e2
Jose Vazquez <vazquezjoseman13@gmail.com>
2023-04-06 21:27:34 +0200

    Completed Lab

--- index.js
+++ index.js
@@ -1,23 +1,61 @@
-const mongoose = require('mongoose');
-
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const Recipe = require("./models/Recipe.model");
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require("./data");
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = "mongodb://0.0.0.0:27017/recipe-app";
 
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI)
-  .then(x => {
+  .then((x) => {
     console.log(`Connected to the database: "${x.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+    return Recipe.deleteMany();
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    Recipe.create({
+      title: "tacos",
+      level: "Easy Peasy",
+      ingredients: ["Tortilla", "Meat"],
+      cuisine: "Mexican",
+      dishType: "breakfast",
+      duration: 30,
+      creator: "Jose",
+    })
+      .then((recipe) => console.log(recipe.title))
+      .catch((recipe) => console.log("An error happened"));
   })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
+  .then(() => {
+    return Recipe.insertMany(data);
+  })
+  .then((recipe) => {
+    return Recipe.findOneAndUpdate(
+      { title: "Rigatoni alla Genovese" },
+      { duration: 100 },
+      { new: true }
+    );
+  })
+  .then((updatedRecipe) => {
+    console.log(`Successfully updated the recipe: ${updatedRecipe.title}`);
+  })
+  .then((recipe) => {
+    return Recipe.deleteOne({ title: "Carrot Cake" });
+  })
+  .then((deletedRecipe) => {
+    console.log(`Successfully deleted the recipe: ${deletedRecipe.title}`);
+  })
+  .catch((error) => {
+    console.error("Error connecting to the database", error);
   });
+process.on("SIGINT", () => {
+  mongoose.connection.close(() => {
+    console.log(
+      "Mongoose default connection disconnected through app termination"
+    );
+    process.exit(0);
+  });
+});
